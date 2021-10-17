7e9a125f25cad243c32322ec0a611c30a8dd3302
kakisakom <68311774+kakisakom@users.noreply.github.com>
2021-10-17 22:38:20 +0900

    testに変更

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ import (
 
 // エントリーポイントはmainという関数として定義する必要がある
 func main() {
+	fmt.Println("koko")
+	var a string = "a"
+	a = "b"
+	a = "c"
+	fmt.Println(a)
 	// デバッグの仕方
 	// https://qiita.com/poramal/items/11912b5533ec8e7dbaac#3-6-%E3%82%B3%E3%83%B3%E3%83%86%E3%83%8A%E5%86%85%E3%81%A7%E3%81%AEgo%E9%96%8B%E7%99%BAhello-world
 	fmt.Println("Hello, Go-lang!!")
